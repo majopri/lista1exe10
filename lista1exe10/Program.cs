2bbd8299aff1a28100a199766e93eca3da1cd0b4
User <User@DESKTOP-G0A3IIV>
2024-04-23 05:08:30 -0300

    Adicionar arquivos de projeto.

---
+++ lista1exe10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exe10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a cotação do dólar:");
+            double cotacaoDolar = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de dólares:");
+            double quantidadeDolares = Convert.ToDouble(Console.ReadLine());
+
+            double valorEmReais = cotacaoDolar * quantidadeDolares;
+
+            Console.WriteLine("O valor correspondente em Reais é: R$ " + valorEmReais);
+        }
+    }
+}
